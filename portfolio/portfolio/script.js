976fbdf9b6e091d7a583cbf1faf9f3dc3c840bc4
MeethBhandari05 <136608986+MeethBhandari05@users.noreply.github.com>
2024-05-29 23:46:41 +0530

    Add files via upload

---
+++ portfolio/portfolio/script.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Message sent!');
+    // You can add more functionality here, like sending the form data to a server
+});
